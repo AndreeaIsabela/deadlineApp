5ab2b850ae9578a02d2b91cff2c133cc1a3e07be
AndreeaIsabela <bala.andreea@yahoo.com>
2021-01-26 23:11:46 +0200

    [main] Add navigator.

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import EventList from "./components/EventList";
+import EventForm from "./components/EventForm";
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={style.navContainer}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={EventList} />
+        <Stack.Screen name="Add Event" component={EventForm} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = StyleSheet.create({
+  navContainer: {
+    textAlign: 'center'
+  }
+})
+
+export default App;
